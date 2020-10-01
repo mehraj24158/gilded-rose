0089db95fd573de975f680b35fb1c4c990c0a970
Mehraj_Ahmed <mehraj24158@gmail.com>
2020-09-30 23:52:24 -0400

    added requirements

--- refactored_gilded_rose.py
+++ refactored_gilded_rose.py
@@ -9,8 +9,14 @@ class Item:
         self.expired = False
         self.degradation_rate = 1
 
+    def bounds(self):
+        if self.quality > 50:
+            self.quality = 50
+        if self.quality < 0 or self.sell_in <=0:
+            self.quality = 0
+
     def update(self):
-        self.sell_in -= self.degradation_rate
+        self.sell_in -= 1
 
         if sell_in <= 0:
             self.expired = True
@@ -22,32 +28,23 @@ class Item:
 
         self.bounds()
 
-    def bounds(self):
-        if self.quality > 50:
-            self.quality = 50
-        if self.quality < 0:
-            self.quality = 0
+    def getQuality(self):
+        return self.quality
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 class ConjuredItem(Item):
-    def __init__():
-        super(Item, self).__init__(self, name = None, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-        self.expired = False
+    def __init__(self, name, sell_in, quality):
+        super(ConjuredItem, self).__init__(name, sell_in, quality)
         self.degradation_rate = 2
     
 
 class LegendaryItem(Item):
-    def __init__():
-        super(Item, self).__init__(self, name = None, sell_in, quality):
-        self.name = name
+    def __init__(self, name, sell_in=None, quality=None):
+        super(LegendaryItem, self).__init__(name, sell_in, quality):
         self.sell_in = None
         self.quality = 80
-        self.expired = False
         self.degradation_rate = 0
 
     def update():
@@ -57,19 +54,34 @@ class LegendaryItem(Item):
         pass
 
 class BackStagePass(Item):
-    def __init__():
+    def __init__(self, name=None, sell_in, quality):
         super(Item, self).__init__(self, name = None, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-        self.expired = False
-        self.degradation_rate = 1
+        self.modifed = False
+        self.modifier = 0
+
+    def modify(self):
+        if self.sell_in <= 10 and self.sell_in > 5:
+            self.modifier = 2
+        if self.sell_in <= 5:
+            self.modifier = 3
 
     def update():
-        
+        self.sell_in -= 1
 
+        if sell_in <= 0:
+            self.expired = True
 
-    
+        if expired:
+            self.quality -= (2*self.degradation_rate)
+        else:
+            self.quality -= self.degradation_rate
+
+        self.bounds()
+        self.modify()
+
+    def getQuality(self):
+        return quality + modifier
+
 
 class Inventory():
     def __init__(self, items):
